c1c8e43dd16bb61774b561c913df494d40c4a195
Mariam Elbeshbeeshy <mariomaelsaeed2@gmail.com>
2026-01-30 22:27:21 +0200

    Finished QRScanView UI, Added QR scanner overlay and custom camera button.

--- lib/views/qr_scan_view.dart
+++ lib/views/qr_scan_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:qr_scanner_overlay/qr_scanner_overlay.dart';
 import 'package:rafeeq_app/helper/constants.dart';
 import 'package:rafeeq_app/widgets/camera_preview.dart';
+import 'package:rafeeq_app/widgets/custom_camera_button.dart';
 
 class QRScanView extends StatefulWidget {
   const QRScanView({super.key});
@@ -16,16 +19,61 @@ class _QRScanViewState extends State<QRScanView> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        foregroundColor: kWhiteBackgroundColor,
-        leading: IconButton(
-          onPressed: () => Navigator.pop(context),
-          icon: Icon(Icons.arrow_back_ios),
-        ),
-      ),
+
       backgroundColor: Colors.black,
-      body: Stack(fit: StackFit.expand, children: [CameraPreview()]),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          CameraPreview(),
+          QRScannerOverlay(
+            overlayColor: const Color.fromARGB(183, 0, 0, 0),
+            scanAreaSize: Size(295.w, 385.h),
+            borderColor: kPrimaryColor,
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Spacer(flex: 1),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: Icon(Icons.arrow_back_ios),
+                      padding: EdgeInsets.only(right: 8.w),
+                      alignment: Alignment.center,
+                      iconSize: 20.w,
+                      color: kWhiteBackgroundColor,
+                    ),
+                    Text(
+                      'امسح رمز الـ QR ',
+                      style: TextStyle(
+                        color: kWhiteBackgroundColor,
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 16.w,
+                    vertical: 8.h,
+                  ),
+                  child: Text(
+                    "وجه الكاميرا نحو رمز ال QR الموجود علي الموقع",
+                    style: TextStyle(color: kWhiteBackgroundColor),
+                  ),
+                ),
+                Spacer(flex: 10),
+                CustomCameraButton(onPressed: () {}),
+                Spacer(flex: 1),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
