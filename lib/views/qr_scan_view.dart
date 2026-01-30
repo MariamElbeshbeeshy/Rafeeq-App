3c0f180ac58300a11dbf3cc649e5f5de980881cd
Mariam Elbeshbeeshy <mariomaelsaeed2@gmail.com>
2026-01-30 19:53:56 +0200

    Add QRScanView and update routing;
    integrate mobile_scanner and permission_handler

---
+++ lib/views/qr_scan_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class QRScanView extends StatelessWidget {
+  const QRScanView({super.key});
+  static String id = 'QR Scan View';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
