90f8f8cd6cdf66411881658b98eeaa45320b9540
Mariam Elbeshbeeshy <mariomaelsaeed2@gmail.com>
2026-06-28 05:49:14 +0300

    feat: Create LibraryCard widget to encapsulate library item display

--- lib/views/library/library_view.dart
+++ lib/views/library/library_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rafeeq_app/helper/constants.dart';
+import 'package:rafeeq_app/widgets/library_card.dart';
 
 class LibraryView extends StatefulWidget {
   const LibraryView({super.key});
@@ -72,93 +73,7 @@ class _LibraryViewState extends State<LibraryView> {
                     final Color dynamicColor =
                         cardColors[index % cardColors.length];
 
-                    return Container(
-                      margin: EdgeInsets.only(bottom: 16.h),
-                      height: 86.h,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(36.r),
-                          bottomRight: Radius.circular(36.r),
-                          topLeft: Radius.circular(8.r),
-                          bottomLeft: Radius.circular(8.r),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withValues(alpha: 0.05),
-                            blurRadius: 8,
-                            offset: const Offset(0, 4),
-                          ),
-                        ],
-                      ),
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 60.w,
-                            height: double.infinity,
-                            decoration: BoxDecoration(
-                              color: dynamicColor,
-                              borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(36.r),
-                                bottomRight: Radius.circular(36.r),
-                                topLeft: Radius.circular(36.r),
-                              ),
-                            ),
-                            alignment: Alignment.center,
-                            child: Text(
-                              "${index + 1}",
-                              style: TextStyle(
-                                fontSize: 26.sp,
-                                fontWeight: FontWeight.bold,
-                                color: const Color(0xFF2D3748),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Padding(
-                              padding: EdgeInsets.symmetric(
-                                horizontal: 12.w,
-                                vertical: 12.h,
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    item["title"],
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      fontSize: 16.sp,
-                                      fontWeight: FontWeight.bold,
-                                      color: const Color(0xFF2D3748),
-                                    ),
-                                  ),
-                                  SizedBox(height: 4.h),
-                                  Text(
-                                    item["level"],
-                                    style: TextStyle(
-                                      fontSize: 13.sp,
-                                      color: Colors.grey.shade600,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-
-                          Padding(
-                            padding: EdgeInsets.only(left: 16.w),
-                            child: const Icon(
-                              Icons.arrow_forward_ios_rounded,
-                              color: kPrimaryColor,
-                              size: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
+                    return LibraryCard(dynamicColor: dynamicColor, item: item, index: index);
                   },
                 ),
               ),
@@ -169,7 +84,6 @@ class _LibraryViewState extends State<LibraryView> {
     );
   }
 
-  // دالة مساعدة لبناء الفلاتر العلوية بشكل متناسق
   Widget _buildFilterChip(String label, int index) {
     bool isSelected = selectedTab == index;
     return GestureDetector(
@@ -200,3 +114,5 @@ class _LibraryViewState extends State<LibraryView> {
     );
   }
 }
+
+
---
+++ lib/widgets/library_card.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:rafeeq_app/helper/constants.dart';
+
+class LibraryCard extends StatelessWidget {
+  const LibraryCard({
+    super.key,
+    required this.dynamicColor,
+    required this.item,
+    required this.index,
+  });
+
+  final int index;
+  final Color dynamicColor;
+  final Map<String, dynamic> item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 16.h),
+      height: 92.h,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(36.r),
+          bottomRight: Radius.circular(36.r),
+          topLeft: Radius.circular(8.r),
+          bottomLeft: Radius.circular(8.r),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.05),
+            blurRadius: 8,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 60.w,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              color: dynamicColor,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(36.r),
+                bottomRight: Radius.circular(36.r),
+                topLeft: Radius.circular(36.r),
+              ),
+            ),
+            alignment: Alignment.center,
+            child: Text(
+              "${index + 1}",
+              style: TextStyle(
+                fontSize: 26.sp,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xFF2D3748),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 12.w,
+                vertical: 12.h,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    item["title"],
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFF2D3748),
+                    ),
+                  ),
+                  SizedBox(height: 4.h),
+                  Text(
+                    item["level"],
+                    style: TextStyle(
+                      fontSize: 13.sp,
+                      color: Colors.grey.shade600,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+    
+          Padding(
+            padding: EdgeInsets.only(left: 16.w),
+            child: const Icon(
+              Icons.arrow_forward_ios_rounded,
+              color: kPrimaryColor,
+              size: 18,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
