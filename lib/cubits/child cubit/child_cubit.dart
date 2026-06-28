7d8a3830eb492543bf4f534e4a308459f8d677cb
Mariam Elbeshbeeshy <mariomaelsaeed2@gmail.com>
2026-06-28 19:06:15 +0300

    feat: Implement ChildCubit and associated states for child data management

---
+++ lib/cubits/child cubit/child_cubit.dart
@@ -0,0 +1,62 @@
+import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:meta/meta.dart';
+import 'package:rafeeq_app/models/level_model.dart';
+import 'package:rafeeq_app/models/user_data_model.dart';
+import 'package:rafeeq_app/services/user_local_services.dart';
+
+part 'child_state.dart';
+
+class ChildCubit extends Cubit<ChildState> {
+  ChildCubit() : super(ChildInitial());
+  final String baseUrl = "https://api-rafiq.runasp.net/api/v1";
+  final Dio dio = Dio();
+
+  Future<void> getChildData(String childId) async {
+    try {
+      final response = await dio.get(
+        "$baseUrl/Profile",
+        options: Options(
+          headers: {
+            "Authorization":
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjYzNDJkMjg3LTllNmYtNDBlNy1hNjA5LTI3NzE4ZjA0M2U5ZiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiLZitin2LPZitmGIiwidHlwZVVzZXIiOiJQYXJlbnQiLCJleHAiOjE4MTg2MTM1MTAsImlzcyI6Imh0dHA6Ly93d3cuZ29vZ2xlLmNvbSIsImF1ZCI6Imh0dHA6Ly93d3cuZ29vZ2xlLmNvbSJ9.tlSWVkfu8i87ZKrHrK1AV9e7nAQseoTLK7k3WFHypuk",
+            //"Authorization": "Bearer ${UserLocalServices().getToken()}",
+            "Accept-Language": "ar",
+          },
+        ),
+      );
+      if (response.statusCode == 200 && response.data != null) {
+        final UserDataModel? childData = await UserLocalServices()
+            .updateUserData(response.data!["data"]);
+        emit(ChildDataLoaded(childData!));
+      }
+    } on DioException catch (e) {
+      emit(ChildDataError("${e.message}"));
+    }
+  }
+
+  Future<void> getChildPortfolio() async {
+    try {
+      final response = await dio.get(
+        "$baseUrl/Portfolio",
+        options: Options(
+          headers: {
+            "Authorization": "Bearer ${UserLocalServices().getToken()}",
+            "accept": "text/plain",
+            "Accept-Language": "ar",
+          },
+        ),
+      );
+      if (response.statusCode == 200 && response.data != null) {
+        final UserDataModel? childData = await UserLocalServices()
+            .updateUserData(response.data!["data"]["profile"]);
+        final List<LevelModel> levels = (response.data!["data"]["map"] as List)
+            .map((levelJson) => LevelModel.fromJson(levelJson))
+            .toList();
+        emit(ChildPortfolioLoaded(childData!, levels));
+      }
+    } on DioException catch (e) {
+      emit(ChildPortfolioError("${e.message}"));
+    }
+  }
+}
--- lib/models/device_model.dart
+++ lib/models/device_model.dart
@@ -1,7 +1,6 @@
 class DeviceModel {
   final String deviceId;
   final String deviceName;
-  final String projectName = "Rafiq";
 
   DeviceModel({required this.deviceId, required this.deviceName});
 }
