d013a528e1bbaf7201424cacd17133af6a289ec9
Mariam Elbeshbeeshy <mariomaelsaeed2@gmail.com>
2026-07-01 04:36:04 +0300

    feat: Add type and content fields to QuestionModel and skillName to QuizStageResponseModel

--- lib/models/questions_model.dart
+++ lib/models/questions_model.dart
@@ -2,15 +2,17 @@ import 'package:rafeeq_app/models/option_model.dart';
 
 class QuestionModel {
   final String id;
+  final int type;
+  final List<String> content;
   final String text;
-  final String? imageUrl; 
   final String? audioUrl;
   final List<OptionModel> options;
 
   QuestionModel({
     required this.id,
+    required this.type,
+    required this.content,
     required this.text,
-    this.imageUrl,
     this.audioUrl,
     required this.options,
   });
@@ -18,8 +20,11 @@ class QuestionModel {
   factory QuestionModel.fromJson(Map<String, dynamic> json) {
     return QuestionModel(
       id: json['id'] ?? '',
+      type: json['type'] ?? 0,
+      content: json['content'] != null
+          ? List<String>.from(json['content'])
+          : [],
       text: json['text'] ?? '',
-      imageUrl: json['imageUrl'],
       audioUrl: json['audioUrl'],
       options: json['options'] != null
           ? List<OptionModel>.from(
--- lib/models/quiz_stage_response_model.dart
+++ lib/models/quiz_stage_response_model.dart
@@ -4,12 +4,14 @@ class QuizStageResponseModel {
   final String sessionId;
   final int currentLevelId;
   final String levelTitle;
+  final String skillName;
   final List<QuestionModel> questions;
 
   QuizStageResponseModel({
     required this.sessionId,
     required this.currentLevelId,
     required this.levelTitle,
+    required this.skillName,
     required this.questions,
   });
 
@@ -18,6 +20,7 @@ class QuizStageResponseModel {
       sessionId: json['sessionId'] ?? '',
       currentLevelId: json['currentLevelId'] ?? 0,
       levelTitle: json['levelTitle'] ?? '',
+      skillName: json['skillName'] ?? '',
       questions: json['questions'] != null
           ? List<QuestionModel>.from(
               json['questions'].map((x) => QuestionModel.fromJson(x)),
